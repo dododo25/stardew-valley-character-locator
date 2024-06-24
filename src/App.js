6034dccdd96334906ddab6210a1262a30ce74ef7
dododo <dmytro.terekhov@outollok.com>
2024-06-24 11:31:31 +0300

    Initial version of a clock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+import Clock from './Clock.js';
 import Map from './Map.js';
 
+const floorMod = (v, mod) => {
+  return (mod + v % mod) % mod;
+};
+
 const App = () => {
+  const [time, setTime] = useState(0);
+
+  const changeTime = v => {
+    setTime(floorMod(time + v, 1440));
+  };
+
   return (
     <div className='wv-100 vh-100 d-flex justify-content-center align-items-center'>
       <Map />
+      <Clock hour={Math.floor(time / 60)} minute={time % 60} onHourChange={v => changeTime(v * 60)} onMinuteChange={v => changeTime(v)} />
     </div>
   );
 }
