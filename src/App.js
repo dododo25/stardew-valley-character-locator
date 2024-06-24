dffe8e9a50742d6cda976babe0b834b92f99ba4c
dododo <dmytro.terekhov@outollok.com>
2024-06-24 12:01:57 +0300

    Create side panel

--- src/App.js
+++ src/App.js
@@ -14,9 +14,20 @@ const App = () => {
   };
 
   return (
-    <div className='wv-100 vh-100 d-flex justify-content-center align-items-center'>
-      <Map />
-      <Clock hour={Math.floor(time / 60)} minute={time % 60} onHourChange={v => changeTime(v * 60)} onMinuteChange={v => changeTime(v)} />
+    <div className='vh-100 overflow-hidden'>
+      <div className='vh-100 d-flex justify-content-center align-items-center'>
+        <Map />
+      </div>
+      <div className='vh-100 d-flex justify-content-end align-items-center position-relative' style={{bottom: '100vh'}}>
+        <div className='h-100 d-flex justify-content-center' style={{width: '16%', backgroundImage: 'url(images/misc/MapBackground.png)', borderLeft: '12px solid black', borderImage: 'url(images/misc/LeftBorder.png) 16 round'}}>
+          <div className='d-flex align-items-center'>
+            <div className='d-flex flex-column align-items-center'>
+              <h2>Clock</h2>
+              <Clock hour={Math.floor(time / 60)} minute={time % 60} onHourChange={v => changeTime(v * 60)} onMinuteChange={v => changeTime(v)} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Clock.js
+++ src/Clock.js
@@ -16,13 +16,13 @@ const Clock = props => {
       </div>
       <div className='d-flex flex-column align-items-center'>
         <div className='d-flex flex-row m-1'>
-          <div className='clock-button me-1' onClick={() => props.onHourChange(-1)}>
+          <div className='clock-button c-pointer me-1' onClick={() => props.onHourChange(-1)}>
             <img src='/images/misc/ButtonFrame.png' alt='hour-minus-one' />
             <div className='position-relative d-flex justify-content-center' style={{bottom: '50%'}}>
               <span style={{lineHeight: 0}}>-1 h</span>
             </div>
           </div>
-          <div className='clock-button' onClick={() => props.onHourChange(1)}>
+          <div className='clock-button c-pointer' onClick={() => props.onHourChange(1)}>
             <img src='/images/misc/ButtonFrame.png' alt='hour-plus-one' />
             <div className='position-relative d-flex justify-content-center' style={{bottom: '50%'}}>
               <span style={{lineHeight: 0}}>+1 h</span>
@@ -30,13 +30,13 @@ const Clock = props => {
           </div>
         </div>
         <div className='d-flex flex-row mx-1'>
-          <div className='clock-button me-1' onClick={() => props.onMinuteChange(-10)}>
+          <div className='clock-button c-pointer me-1' onClick={() => props.onMinuteChange(-10)}>
             <img src='/images/misc/ButtonFrame.png' alt='minute-minus-one' />
             <div className='position-relative d-flex justify-content-center' style={{bottom: '50%'}}>
               <span style={{lineHeight: 0}}>-10 m</span>
             </div>
           </div>
-          <div className='clock-button' onClick={() => props.onMinuteChange(10)}>
+          <div className='clock-button c-pointer' onClick={() => props.onMinuteChange(10)}>
             <img src='/images/misc/ButtonFrame.png' alt='minute-plus-one' />
             <div className='position-relative d-flex justify-content-center' style={{bottom: '50%'}}>
               <span style={{lineHeight: 0}}>+10 m</span>
